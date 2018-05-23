161c92d5ee153245eef483564d9b75576d94021b
Paul Swartz <paul@paulswartz.net>
2018-05-23 11:27:15 -0400

    initial commit (mix new)

---
+++ lib/parking_tweets.ex
@@ -0,0 +1,18 @@
+defmodule ParkingTweets do
+  @moduledoc """
+  Documentation for ParkingTweets.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ParkingTweets.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/parking_tweets_test.exs
@@ -0,0 +1,8 @@
+defmodule ParkingTweetsTest do
+  use ExUnit.Case
+  doctest ParkingTweets
+
+  test "greets the world" do
+    assert ParkingTweets.hello() == :world
+  end
+end
