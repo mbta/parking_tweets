8287e85fb1d85575b7e46db81fd987e7b627e315
Paul Swartz <paul@paulswartz.net>
2019-04-17 13:31:37 -0400

    revert changes to use scheduled time
    Introduces too many bugs.

--- test/parking_tweets/updated_garages_test.exs
+++ test/parking_tweets/updated_garages_test.exs
@@ -77,23 +77,6 @@ defmodule ParkingTweets.UpdatedGaragesTest do
       _ = maybe_send_tweet(state, @now)
       assert_receive {:tweet, _}
     end
-
-    test "uses the scheduled time for the tweet, rather than the current time", %{state: state} do
-      current =
-        Enum.reduce(
-          [Garage.new(name: "Garage", status: "FULL", utilization: 1, capacity: 1)],
-          state.current,
-          &GarageMap.put(&2, &1)
-        )
-
-      last_tweet_at = %{@now | hour: 13, minute: 2}
-      now = %{@now | hour: 13, minute: 31}
-      state = %{state | current: current, last_tweet_at: last_tweet_at}
-      new_state = maybe_send_tweet(state, now)
-      assert_receive {:tweet, tweet}
-      assert tweet =~ "1:30 PM"
-      assert next_scheduled_time(state) != next_scheduled_time(new_state)
-    end
   end
 
   describe "send_tweet/2" do
@@ -109,7 +92,7 @@ defmodule ParkingTweets.UpdatedGaragesTest do
         )
 
       state = %{state | current: current}
-      new_state = send_tweet(state, @now, @now)
+      new_state = send_tweet(state, @now)
       assert new_state.previous == state.current
       assert new_state.last_tweet_at == @now
       assert_receive {:tweet, text}
@@ -129,10 +112,10 @@ defmodule ParkingTweets.UpdatedGaragesTest do
       }
 
       state = update_garages(state, [SampleEvents.reset()])
-      state = send_tweet(state, @now, @now)
+      state = send_tweet(state, @now)
       assert_receive {:tweet, _}
       state = update_garages(state, [%Event{event: "update", data: Jason.encode!(update)}])
-      _state = send_tweet(state, @now, @now)
+      _state = send_tweet(state, @now)
       assert_receive {:tweet, text}
       assert text =~ "Alewife"
       assert text =~ "Braintree"
@@ -152,32 +135,10 @@ defmodule ParkingTweets.UpdatedGaragesTest do
         )
 
       state = %{state | current: current}
-      new_state = send_tweet(state, @now, @now)
+      new_state = send_tweet(state, @now)
       refute_receive {:tweet, _}
       assert new_state.previous == state.current
       assert new_state.current == state.current
-      assert new_state.last_tweet_at == @now
-    end
-
-    test "uses the scheduled time for the contents of the tweet", %{state: state} do
-      current =
-        GarageMap.put(
-          state.current,
-          Garage.new(
-            id: "A",
-            name: "A",
-            utilization: 1,
-            capacity: 1,
-            updated_at: @now
-          )
-        )
-
-      state = %{state | current: current}
-
-      scheduled_time = %{@now | hour: 13, minute: 30}
-      new_state = send_tweet(state, scheduled_time, @now)
-      assert_receive {:tweet, tweet}
-      assert tweet =~ "1:30 PM"
       assert new_state.last_tweet_at == @now
     end
   end
